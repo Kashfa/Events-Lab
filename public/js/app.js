025a4265ad6153041b326cf1c4f5c20952bd2aae
Kashfa <cyan82@hotmail.com>
2018-04-17 14:01:01 +0100

    added genre, added reset and attempted to add validation

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const handleFormSubmit = function (event) {
   event.preventDefault();
   const inputTitle = this.title.value;
   const inputAuthor = this.author.value;
+  const inputGenre = this.genre.value;
   // const categorySelect = this.category.value;
 
   let options = document.getElementsByName('category');
@@ -14,19 +15,34 @@ const handleFormSubmit = function (event) {
 
   const bookDetails = document.querySelector('#book-list')
   // bookDetails.textContent = `Title: ${inputTitle}\n Author: ${inputAuthor}`;
-  addBook(inputTitle, inputAuthor, categorySelect, bookDetails);
+  addBook(inputTitle, inputAuthor, categorySelect, inputGenre, bookDetails);
   console.dir(categorySelect);
 
 
 }
 
-const addBook = function (title, author, category, container) {
+const addBook = function (title, author, category, subject, container) {
 let bookListItem = document.createElement('li');
-bookListItem.textContent = `Title: ${title} Author: ${author} Category: ${category}`;
+bookListItem.textContent = `Title: ${title} Author: ${author} Category: ${category} Subject: ${subject}`;
 container.appendChild(bookListItem);
+
+document.getElementById("book-form").reset();
 
 }
 
+const validate = function () {
+  if (document.form.title === "") {
+    alert("Please enter a title");
+    document.form.title.focus();
+    return false;
+  }
+  if (document.form.author === "" ) {
+     alert("Please enter an author");
+     document.form.author.focus();
+     return false;
+  }
+  return true;
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -34,5 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#book-form');
   form.addEventListener('submit', handleFormSubmit);
+
 
 });
